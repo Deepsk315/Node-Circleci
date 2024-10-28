4de22aabfd400b7b9328bf53b7fa6d0391e41b25
Deepak <deepsk3697@gmail.com>
2024-10-28 16:31:06 +0530

    check for fail case

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const app = express();
 const PORT = 3001
 
 app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+    res.status(200).send('Hello, World!');
 });
 
+app.get('/test-api',(req,res)=>{
+    let {a,b} = req.query;
+    const sum = parseInt(a)+parseInt(b)
+    return res.status(200).send(sum.toString)
+})
+
 const server = app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = { app, server };
+
+
--- app.test.js
+++ app.test.js
@@ -3,13 +3,23 @@ const request = require('supertest');
 const { app, server } = require('./app');
 
 describe('GET /hello', () => {
-    afterAll(done => {
-        server.close(done);  // Close the server after tests
-    });
 
     it('responds with Hello, World!', async () => {
         const response = await request(app).get('/hello');
         expect(response.text).toBe('Hello, World!');
         expect(response.status).toBe(200);
     });
+});
+
+describe('GET /test-api',()=>{
+    it('Obtains the addition of two numbers',async()=>{
+        const response = await request(app).get('/test-api?a=10&b=20');
+        console.log('received resposne : ',response)
+        expect(response.text).toBe('30');
+        expect(response.status).toBe(200)
+    })
+})
+
+afterAll(done => {
+    server.close(done);
 });
